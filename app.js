e27c83fd361ba92bcde9310f2c2606f9e899fe37
abhishek <nabhi531@gmail.com>
2020-09-24 01:26:34 +0530

    project completed

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const app = express();
+
+// Serves Express Yourself website
+app.use(express.static('public'));
+
+const { getElementById, getIndexById, updateElement,
+        seedElements, createElement } = require('./utils');
+
+const expressions = [];
+seedElements(expressions, 'expressions');
+const animals = [];
+seedElements(animals, 'animals');
+
+const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
+app.get('/expressions', (req, res, next) => {
+  res.send(expressions);
+});
+
+app.get('/expressions/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  if (foundExpression) {
+    res.send(foundExpression);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.put('/expressions/:id', (req, res, next) => {
+  const expressionIndex = getIndexById(req.params.id, expressions);
+  if (expressionIndex !== -1) {
+    updateElement(req.params.id, req.query, expressions);
+    res.send(expressions[expressionIndex]);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.post('/expressions', (req, res, next) => {
+  const receivedExpression = createElement('expressions', req.query);
+  if (receivedExpression) {
+    expressions.push(receivedExpression);
+    res.status(201).send(receivedExpression);
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.delete('/expressions/:id', (req, res, next) => {
+  const expressionIndex = getIndexById(req.params.id, expressions);
+  if (expressionIndex !== -1) {
+    expressions.splice(expressionIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
+app.get('/animals',(req,res,next)=>{
+  res.send(animals)
+})
+app.get('/animals/:id',(req,res,next)=>{
+  const animal = getElementById(req.params.id,animals)
+  if(animals){
+    res.send(animal)
+  }else{
+    res.status(404).send('NOT FOUND')
+  }
+})
+app.put('/animals/:id',(req,res,next)=>{
+  const eve = getElementById(req.params.id,animals)
+  if(eve){
+    const animalexp = updateElement(req.params.id,req.query,animals)
+    res.send(animalexp)
+    
+  }else{
+    res.status(404).send('NOT FOUND')
+  }
+})
+app.post('/animals',(req,res,next)=>{
+  const add = createElement('animals',req.query)
+  if(add){
+    animals.push(add)
+    res.status(201).send(add)
+  }else{
+    res.status(400).send('NOT FOUND')
+  }
+})
+app.delete('/animals/:id',(req,res,next)=>{
+  const abc = getElementById(req.params.id,animals)
+  if(abc){
+    animals.splice(abc,1)
+    res.status(204).send()
+  }else{
+    res.status(404).send()
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`); 
+});
